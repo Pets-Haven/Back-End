04657cb884543cc3700e44af507013a6d14b4e72
omarosama203 <omarkukri203@gmail.com>
2024-04-16 00:05:54 +0200

    nullable refrences in cartDto

--- DTO/CartDTO.cs
+++ DTO/CartDTO.cs
@@ -3,10 +3,10 @@
     public class CartDTO
     {
         public int productId { get; set; }
-        public string productName { get; set; }
-        public Double productPrice { get; set; }
-        public string productImage { get; set; }
-        public int productQuantity { get; set; }
+        public string? productName { get; set; }
+        public Double? productPrice { get; set; }
+        public string? productImage { get; set; }
+        public int? productQuantity { get; set; }
         public int cartQuantity { get; set; } 
     }
 }
